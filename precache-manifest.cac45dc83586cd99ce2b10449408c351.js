462e8404a5d6f23531223f52a9a80082df4c0c91
rayhantif <rayhan.zharfan.tif18@polban.ac.id>
2020-03-04 13:09:45 +0700

    first commit

---
+++ precache-manifest.cac45dc83586cd99ce2b10449408c351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f3366999ed6059c89580128efa59bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "762b8271143109055110",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "cb8fd4dabe232ce5e0ad",
+    "url": "/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "762b8271143109055110",
+    "url": "/static/js/2.25f250a2.chunk.js"
+  },
+  {
+    "revision": "cb8fd4dabe232ce5e0ad",
+    "url": "/static/js/main.abaef1c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
